c79f9f2418bed89802d85fbd72163a2bc7577c84
Alex SIEYADJEU <alex.sieyadjeu@lyaprotect.com>
2023-05-29 12:51:59 +0200

    init infra kafka

--- infrastructure/kafka/kafka-config-data/src/main/java/io/albrains/cleanarchitecture/unicontext/infrastructure/Main.java
+++ infrastructure/kafka/kafka-config-data/src/main/java/io/albrains/cleanarchitecture/unicontext/infrastructure/Main.java
@@ -1,4 +1,4 @@
-package io.albrains.cleanarchitecture.unicontext;
+package io.albrains.cleanarchitecture.unicontext.infrastructure;
 
 public class Main {
     public static void main(String[] args) {
